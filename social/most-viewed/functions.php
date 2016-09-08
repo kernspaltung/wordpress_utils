c06d169ac4dffab679bd76d7853b797db97733e9
furenku <furenku@gmail.com>
2016-09-08 01:56:07 -0500

    most viewed posts

---
+++ social/most-viewed/functions.php
@@ -0,0 +1,43 @@
+<?php
+
+function wu_set_post_views($postID) {
+    $count_key = 'wu_post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+//To keep the count accurate, lets get rid of prefetching
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
+
+function wu_track_post_views ($post_id) {
+    if ( !is_single() ) return;
+    if ( empty ( $post_id) ) {
+        global $post;
+        $post_id = $post->ID;
+    }
+    wu_set_post_views($post_id);
+}
+add_action( 'wp_head', 'wu_track_post_views');
+
+
+
+function wu_get_post_views($postID){
+    $count_key = 'wu_post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+        return "0 View";
+    }
+    return $count.' Views';
+}
+
+
+?>
---
+++ social/most-viewed/single.php
@@ -0,0 +1,1 @@
+<?php echo wu_get_post_views(get_the_ID()); ?>
