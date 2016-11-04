cbf175ea23b04bf087259ffcef4fc4cd611b8eb6
josecaos <jazzvoon@gmail.com>
2016-11-03 20:38:00 -0600

    Agrego funcion para tener varios excerpts custom

--- config/custom-excerpt-length.php
+++ config/custom-excerpt-length.php
@@ -1,6 +1,40 @@
 //functions.php
-/// tamano de exctracto
+/// tamano de exctracto general
 function custom_excerpt_length( $length ) {
    return 18;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length');
+
+
+
+/// Para tener tamano custom de extracto
+
+function excerpt($limit) {
+      $excerpt = explode(' ', get_the_excerpt(), $limit);
+      if (count($excerpt)>=$limit) {
+        array_pop($excerpt);
+        $excerpt = implode(" ",$excerpt).'...';
+      } else {
+        $excerpt = implode(" ",$excerpt);
+      } 
+      $excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
+      return $excerpt;
+    }
+
+    function content($limit) {
+      $content = explode(' ', get_the_content(), $limit);
+      if (count($content)>=$limit) {
+        array_pop($content);
+        $content = implode(" ",$content).'...';
+      } else {
+        $content = implode(" ",$content);
+      } 
+      $content = preg_replace('/\[.+\]/','', $content);
+      $content = apply_filters('the_content', $content); 
+      $content = str_replace(']]>', ']]&gt;', $content);
+      return $content;
+    }
+
+//Se manda a llamar la funcion con el parametro del tamano del extracto
+//ej:
+the_excerpt(9);
